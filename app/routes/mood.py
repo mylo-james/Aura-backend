d4c5c50c832691237839e41ce151b62f4e0d5078
Zachary Henderson <zachary.henderson114@gmail.com>
2020-07-25 18:36:03 -0500

    new routes for mood

--- app/routes/mood.py
+++ app/routes/mood.py
@@ -23,3 +23,39 @@ def create():
   db.session.commit()
 
   return {'message': 'done'}
+
+@bp.route('/user/<id>/page/<page>')
+def getUserMoods(id, page):
+  user_id = int(id)
+  page = int(page)
+  moods = Mood.query.filter(Mood.user_id == user_id).order_by(Mood.created_at.desc()).offset(page).limit(3)
+  mood_list = []
+  for mood in moods:
+    mood_list.append(mood.to_dict())
+  return {'moods': mood_list}
+
+@bp.route('/<id>', methods=['DELETE'])
+def delete(id):
+  int_id = int(id)
+  print(id)
+  actions = Action.query.filter(Action.mood_id == int_id).all()
+  mood = Mood.query.filter(Mood.id == int_id).first()
+  for  action in actions:
+    db.session.delete(action)
+  db.session.delete(mood)
+  db.session.commit()
+  return mood.to_dict()
+
+
+@bp.route('/<id>', methods=['PUT'])
+def put(id):
+  data = request.json
+  int_id = int(id)
+  mood = Mood.query.filter(Mood.id == int_id).first()
+  mood.title = data['title']
+  mood.content = data['content']
+  db.session.commit()
+
+  return mood.to_dict()
+
+  
