ff03b8e5d56e02bdd9bb5bb2713618a3a6305870
Zachary Henderson <zachary.henderson114@gmail.com>
2020-07-29 14:33:57 -0500

    added database URL

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Configuration:
-    SQLALCHEMY_DATABASE_URI = "postgresql://aura_admin:password@localhost/aura_app"
+    SQLALCHEMY_DATABASE_URI = "postgres://ioftryihlxwpwo:2734de29711c82cc0d32f3ec7d5a4e89eaf5bd0aef2f66305380606128259dc6@ec2-18-235-109-97.compute-1.amazonaws.com:5432/d9dkpecupraiat"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "jd73%38*ljh93^%#4"
