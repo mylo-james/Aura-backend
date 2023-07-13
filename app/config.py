fc0656a4a2629e63bbaa7ee004bef68412664316
Mylo James <mylo.james114@gmail.com>
2023-07-13 13:23:52 -0500

    change

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Configuration:
-    SQLALCHEMY_DATABASE_URI = "postgresql://uppfbfptsensrw:d3e78e896db4d6270486bfb2b501623b0839250ab01ca1c761e8b308df0e7e5c@ec2-44-215-40-87.compute-1.amazonaws.com:5432/de1g4h64japaep"
+    SQLALCHEMY_DATABASE_URI = "postgresql://ioftryihlxwpwo:2734de29711c82cc0d32f3ec7d5a4e89eaf5bd0aef2f66305380606128259dc6@ec2-18-235-109-97.compute-1.amazonaws.com:5432/d9dkpecupraiat"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "jd73%38*ljh93^%#4"
